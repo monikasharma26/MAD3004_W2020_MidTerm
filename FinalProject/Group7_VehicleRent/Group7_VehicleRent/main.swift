36d736a91edb51a3d8b1e1e5093fbcd21f406aab
monika <monika>
2020-02-13 18:36:09 -0500

    [Monika]Project Created

---
+++ FinalProject/Group7_VehicleRent/Group7_VehicleRent/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Group7_VehicleRent
+//
+//  Created by S@i on 2020-02-13.
+//  Copyright © 2020 S@i. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+

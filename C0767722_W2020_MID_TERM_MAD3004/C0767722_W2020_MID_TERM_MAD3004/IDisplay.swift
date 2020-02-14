832342d7b6252db465703cf54047412ecd272aab
monika <monika>
2020-02-14 00:46:47 -0500

    [Monika] Protocol Added

---
+++ C0767722_W2020_MID_TERM_MAD3004/C0767722_W2020_MID_TERM_MAD3004/IDisplay.swift
@@ -0,0 +1,13 @@
+//
+//  IDisplay.swift
+//  C0767722_W2020_MID_TERM_MAD3004
+//
+//  Created by S@i on 2020-02-14.
+//  Copyright © 2020 S@i. All rights reserved.
+//
+
+import Foundation
+//Created Protocol
+protocol IDisplay {
+    func display()
+}

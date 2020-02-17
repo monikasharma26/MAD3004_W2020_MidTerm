c7203e32e18f65a9af1f71e35f2b9f445ffa209b
monika <monika>
2020-02-16 22:39:31 -0500

    [Monika] Extension for Units, gb, Minutes updated

--- C0767722_W2020_MID_TERM_MAD3004/C0767722_W2020_MID_TERM_MAD3004/Extesnsions/Date+Extension.swift
+++ C0767722_W2020_MID_TERM_MAD3004/C0767722_W2020_MID_TERM_MAD3004/Extesnsions/Date+Extension.swift
@@ -12,7 +12,7 @@ extension Date
     public func getForamttedDate() -> String
     {
         let dateFormatterPrint = DateFormatter()
-        dateFormatterPrint.dateFormat = "YYYY-MM-dd"
+        dateFormatterPrint.dateFormat = "dd-MM-YYYY"
         let formattedDate = dateFormatterPrint.string(from: self)
         return formattedDate
     }

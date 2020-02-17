b9353945630181629245e20aa3884a0ba4372853
monika <monika>
2020-02-16 23:42:12 -0500

    [Monika] Custom Error Added

---
+++ C0767722_W2020_MID_TERM_MAD3004/C0767722_W2020_MID_TERM_MAD3004/Errors/CustomerError.swift
@@ -0,0 +1,13 @@
+//
+//  CustomerError.swift
+//  C0767722_W2020_MID_TERM_MAD3004
+//
+//  Created by S@i on 2020-02-16.
+//  Copyright © 2020 S@i. All rights reserved.
+//
+
+import Foundation
+enum CustomerError:Error
+{
+    case emailInvalid, mobileInvalid
+}
